13f2ec92fb73a5a0fb10d07c3d5ac860c5669e30
Dmitriy Galtsev <mr.dimenter@gmail.com>
2023-02-17 01:10:49 +0400

    feat: added logs

--- fraud/src/main/java/org/example/controller/FraudController.java
+++ fraud/src/main/java/org/example/controller/FraudController.java
@@ -1,6 +1,7 @@
 package org.example.controller;
 
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.example.FraudCheckResponse;
 import org.example.FraudCheckService;
 import org.springframework.web.bind.annotation.*;
@@ -8,6 +9,7 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("api/v1/fraud-check")
 @AllArgsConstructor
+@Slf4j
 public class FraudController {
     private final FraudCheckService fraudCheckService;
 
@@ -16,7 +18,7 @@ public class FraudController {
             @PathVariable("customerId") Integer customerId) {
 
         boolean isFraudulentCustomer =  fraudCheckService.isFraudulentCustomer(customerId);
-
+        log.info("fraud check for customer {}", customerId);
         return new FraudCheckResponse(isFraudulentCustomer);
     }
 
