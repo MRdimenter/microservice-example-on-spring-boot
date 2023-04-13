9c5aaea56aec516626f0fdf93fe76bc3620865cd
Dmitriy Galtsev <mr.dimenter@gmail.com>
2023-04-13 23:49:28 +0400

    feat: TopicExchange, Queue and Binding beans created

--- notification/src/main/java/org/example/notification/NotificationConfig.java
+++ notification/src/main/java/org/example/notification/NotificationConfig.java
@@ -1,12 +1,36 @@
 package org.example.notification;
 
 import lombok.Getter;
+import org.springframework.amqp.core.Binding;
+import org.springframework.amqp.core.BindingBuilder;
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.core.TopicExchange;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @Getter
 public class NotificationConfig {
+
+    @Bean
+    public TopicExchange internalTopicExchange() {
+        return new TopicExchange(this.internalExchange);
+    }
+
+    @Bean
+    public Queue notificationQueue() {
+        return new Queue(this.notificationQueue);
+    }
+
+    @Bean
+    public Binding internalToNotificationBinding() {
+        return BindingBuilder
+                .bind(notificationQueue())
+                .to(internalTopicExchange())
+                .with(this.internalNotificationRoutingKeys);
+    }
+
     @Value("${rabbitmq.exchanges.internal}")
     private String internalExchange;
 
