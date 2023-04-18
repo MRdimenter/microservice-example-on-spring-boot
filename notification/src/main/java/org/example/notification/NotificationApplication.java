8b210f4c75bf02a570012d38ab60fb092690c5c0
Dmitriy Galtsev <mr.dimenter@gmail.com>
2023-04-19 01:38:55 +0400

    feat: create publish to RabbitMQ

--- notification/src/main/java/org/example/notification/NotificationApplication.java
+++ notification/src/main/java/org/example/notification/NotificationApplication.java
@@ -1,11 +1,33 @@
 package org.example.notification;
 
+import org.example.RabbitMQMessageProducer;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
-@SpringBootApplication
+@SpringBootApplication(
+        scanBasePackages = {
+                "org.example.notification",
+                "org.example"
+        }
+)
 public class NotificationApplication {
     public static void main(String[] args) {
         SpringApplication.run(NotificationApplication.class);
     }
+
+    @Bean
+    CommandLineRunner commandLineRunner(
+            RabbitMQMessageProducer rabbitMQMessageProducer,
+            NotificationConfig notificationConfig
+    ) {
+        return args -> {
+            rabbitMQMessageProducer.publish(
+                    "foo",
+                    notificationConfig.getInternalExchange(),
+                    notificationConfig.getInternalNotificationRoutingKeys());
+        };
+    }
+
 }
